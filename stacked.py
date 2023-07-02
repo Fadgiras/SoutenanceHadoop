9ea5b45509b1bf68597adf33e077e631c2bc80d0
Fadgiras <mayeuxgudinaflorian@gmail.com>
2023-07-03 00:54:33 +0200

    Updated the results, and the stacked chart

--- stacked.py
+++ stacked.py
@@ -9,10 +9,10 @@ with open("result.txt") as fichier:
     for ligne in fichier:
         # Split the line into a list of values
         valeurs = ligne.split(",")
-        gare = valeurs[0]
+        ligne = valeurs[3]
         montant = int(valeurs[-1])
         
-        if "AEROPORT CHARLES DE GAULLE" in gare:
+        if "A" in ligne or "B" in ligne or "C" in ligne or "D" in ligne :
             montants_Aeroport += montant
         else:
             montants_autre += montant
@@ -24,9 +24,9 @@ values = [montants_Aeroport, montants_autre]
 pourcent = (montants_Aeroport/(montants_Aeroport+montants_autre)*100).__str__()
 pourcent = round(float(pourcent), 2)
 
-plt.xlabel('Proportion aéroport/total : ' + pourcent.__str__() + '%')
+plt.xlabel('Proportion A B C & D/total : ' + pourcent.__str__() + '%')
 
-ax.bar("Voyageurs", montants_Aeroport, 0.6, label="Aeroport", bottom=0)
+ax.bar("Voyageurs", montants_Aeroport, 0.6, label="A B C D", bottom=0)
 ax.bar("Voyageurs", montants_autre, 0.6, label="Autres", bottom=montants_Aeroport)
 
 # Add labels on the center of the bars
@@ -34,6 +34,6 @@ ax.text("Voyageurs", montants_Aeroport/2, str(montants_Aeroport), ha='center', v
 ax.text("Voyageurs", montants_Aeroport + montants_autre/2, str(montants_autre), ha='center', va='bottom')
 
 
-ax.set_title("Nombre de voyageurs en destination de CDG par rapport au reste du trafic")
+ax.set_title("Nombre de voyageurs prenant les lignes A,B C ou D\n par rapport au reste du trafic")
 ax.legend(loc="upper right")
 plt.show()
